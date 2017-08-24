b1a0d76011ffcdfc00e4f7b0db95d18c83b28e95
deadlybutter <thedeadlybutter@gmail.com>
2017-08-24 15:46:19 -0400

    Fixing card

--- client/actions/card.js
+++ client/actions/card.js
@@ -2,22 +2,23 @@ export const EXPAND_CARD = 'EXPAND_CARD';
 export const CLOSE_CARD = 'CLOSE_CARD';
 export const DELETE_CARD = 'DELETE_CARD';
 
-export function expandCard(cardId, parent) {
-  return { type: EXPAND_CARD, cardId, parent };
+export function expandCard(cardId) {
+  return { type: EXPAND_CARD, cardId };
 }
 
-export function closeCard(cardId, parent) {
-  return { type: CLOSE_CARD, cardId, parent };
+export function closeCard(cardId) {
+  return { type: CLOSE_CARD, cardId };
 }
 
-export function toggleCard(cardId, parent) {
+export function toggleCard(cardId) {
   return (dispatch, getState) => {
     const card = getState().card.items[cardId];
+
     if (! card || ! card.expand) {
-      return dispatch(expandCard(cardId, parent));
+      return dispatch(expandCard(cardId));
     }
 
-    return dispatch(closeCard(cardId, parent));
+    return dispatch(closeCard(cardId));
   }
 }
 
--- client/reducers/card.js
+++ client/reducers/card.js
@@ -2,61 +2,33 @@ import {
   EXPAND_CARD, CLOSE_CARD, DELETE_CARD,
 } from '../actions';
 
-const ensureCard = (cardId, state) => {
-  if (! cardId || !state) return;
-
-  if (! state.items[cardId]) {
-    return {
-      ...state,
-      items: {
-        [cardId]: {
-          expand: false,
-        },
-      },
-    };
-  }
-
-  return { ...state };
-};
-
 const card = (state = {}, action) => {
-  const type = action.type || '';
-  const cardId = action.cardId;
-  const parent = action.parent;
-  const safeState = ensureCard(cardId, state);
-
-  switch(type) {
+  switch(action.type) {
     case EXPAND_CARD:
       return {
-        ...safeState,
-        items: Object.keys(safeState.items).reduce((items, id) => {
-          const card = safeState.items[id];
-
-          return {
-            ...items,
-            [id]: {
-              ...card,
-              expand: id === cardId || id === parent,
-            },
-          };
-        }, {}),
+        ...state,
+        items: {
+          ...state.items,
+          [action.cardId]: {
+            expand: true,
+          },
+        },
       };
 
     case CLOSE_CARD:
       return {
-        ...safeState,
+        ...state,
         items: {
-          [cardId]: {
-            ...safeState.items[cardId],
+          ...state.items,
+          [action.cardId]: {
             expand: false,
           },
-        }
+        },
       };
 
     case DELETE_CARD:
       const deleteState = { ...state };
-      delete deleteState.items[cardId];
-
+      delete deleteState[action.cardId];
       return deleteState;
 
     default: return state;
